ca2f723c2dd3f0f33f045e89bca6ee38a0be1583
jaouad mhamdi <98467197+jm-hamdi@users.noreply.github.com>
2024-09-21 17:14:47 +0100

    project build

--- obj/Debug/net8.0/HRManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/HRManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f46d61c463bee59794d7da4f7b72b10e5dcadfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
